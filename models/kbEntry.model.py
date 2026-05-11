8204a47e8681b784d9800bf5694d4b69f35c3d8a
ksrichakri <142304892+ksrichakri@users.noreply.github.com>
2026-05-11 11:04:10 +0530

    Data model of Knowledge Base entry and search query

---
+++ models/kbEntry.model.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel , Field
+from typing import List , Optional
+
+class Entry(BaseModel):
+    title : str = Field(...,
+                        min_length = 5,
+                        max_length = 50,
+                        description="Title of entry")
+    category: str = Field(...,
+                          min_length = 5,
+                          description="Category of problem")
+    problem: str = Field(...,
+                         min_length = 10 , 
+                         max_length = 200,
+                         description="The issue raised")
+    solution : str = Field(... ,
+                           min_length = 20,
+                           description="Solution provided to the problem raised")
+    codeSnippet: Optional[str] = ""
+    tags:Optional[List[str]] = []
